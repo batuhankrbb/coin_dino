a604eb771a901ac4dbf1cbbe2a0e4c9822d09a65
batuhankrbb <batuhankarababa@gmail.com>
2021-05-20 15:54:03 +0300

    add: coin search | update: test

--- lib/features/search/domain/repository_contract/i_repository_contract.dart
+++ lib/features/search/domain/repository_contract/i_repository_contract.dart
@@ -1,6 +1,8 @@
 import 'package:coin_dino/core/result_types/result.dart';
+import 'package:coin_dino/features/search/domain/entity/search_coin_entity.dart';
 import 'package:coin_dino/features/search/domain/entity/search_trend_entity.dart';
 
-abstract class ISearchRepository{
+abstract class ISearchRepository {
   Future<Result<SearchTrendEntity>> getAllTrends();
+  Future<Result<List<SearchCoinEntity>>> getCoinsBySearch(String text);
 }
