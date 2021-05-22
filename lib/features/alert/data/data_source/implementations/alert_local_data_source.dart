165afc18dd1041490942c00fe46f7608b674b514
batuhankrbb <batuhankarababa@gmail.com>
2021-05-22 10:35:18 +0300

    update: alert local data source

--- lib/features/alert/data/data_source/implementations/alert_local_data_source.dart
+++ lib/features/alert/data/data_source/implementations/alert_local_data_source.dart
@@ -1,35 +1,47 @@
+import 'package:coin_dino/core/hive/hive_constants.dart';
+import 'package:coin_dino/core/hive/hive_helper.dart';
 import 'package:coin_dino/features/alert/data/data_source/contracts/i_alert_local_data_source.dart';
+import 'package:coin_dino/features/alert/data/exception_handling/exceptions/alert_exceptions.dart';
 import 'package:coin_dino/features/alert/data/model/alert_model.dart';
 
-class AlertLocalDataSource implements IAlertLocalDataSource{
+class AlertLocalDataSource implements IAlertLocalDataSource {
+  var _hiveHelper = HiveHelper();
+
   @override
-  Future<List<AlertModel>> getAllAlerts() {
-    // TODO: implement getAllAlerts
-    throw UnimplementedError();
+  Future<List<AlertModel>> getAllAlerts() async {
+    try {
+      var alerts =
+          await _hiveHelper.getAll<AlertModel>(HiveConstants.BOX_ALERTS);
+      return alerts;
+    } catch (e) {
+      throw AlertException.allAlertsFetchingException();
+    }
   }
 
   @override
-  Future<List<AlertModel>> saveAlertAlerts() {
-    // TODO: implement saveAlertAlerts
-    throw UnimplementedError();
+  Future<void> deleteAlert(String alertID) async{
+    try {
+      //TODO İMPLEMENT EDİLECEK
+    } catch (e) {
+      throw AlertException.alertDeletingException();
+    }
   }
 
   @override
-  Future<void> deleteAlert(String alertID) {
-      // TODO: implement deleteAlert
-      throw UnimplementedError();
+  Future<void> saveAlert(AlertModel alertModel) async {
+    try {
+      //TODO İMPLEMENT EDİLECEK
+    } catch (e) {
+      throw AlertException.alertSavingException();
     }
-  
-    @override
-    Future<void> saveAlert(AlertModel alertModel) {
-      // TODO: implement saveAlert
-      throw UnimplementedError();
-    }
-  
-    @override
-    Future<void> updateAlert(AlertModel alertModel) {
-    // TODO: implement updateAlert
-    throw UnimplementedError();
   }
-  
+
+  @override
+  Future<void> updateAlert(AlertModel alertModel) async {
+    try {
+      //TODO İMPLEMENT EDİLECEK
+    } catch (e) {
+      throw AlertException.alertUpdatingException();
+    }
+  }
 }
