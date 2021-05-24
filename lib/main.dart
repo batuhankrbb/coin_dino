cc2abaa4029dca24069c916934597e56948dd66a
Sercan Kaya <sercan.kaya@icloud.com>
2021-05-24 09:29:28 +0300

    dsada

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
-
+import 'dart:convert';
 
 import 'package:coin_dino/core/hive/hive_helper.dart';
-import 'package:coin_dino/features/alert/data/data_source/implementations/alert_local_data_source.dart';
-import 'package:coin_dino/features/alert/data/data_source/implementations/alert_remote_data_source.dart';
-import 'package:coin_dino/features/alert/data/exception_handling/exception_handler.dart';
-import 'package:coin_dino/features/alert/data/exception_handling/exceptions/alert_exceptions.dart';
-import 'package:coin_dino/features/alert/data/repository/alert_repository.dart';
-import 'package:coin_dino/features/alert/domain/repository_contract/i_alert_repository.dart';
-
+import 'package:coin_dino/features/preferences/data/exception_handling/exception_handler.dart';
 import 'package:coin_dino/features/preferences/data/implementations/preferences_local_data_source.dart';
 import 'package:coin_dino/features/preferences/data/repository/preference_repository.dart';
 import 'package:coin_dino/features/preferences/domain/repository_contract/i_preference_repository.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() async {
   await HiveHelper.shared.setUpHive();
@@ -46,11 +40,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  IAlertRepository alertRepo = AlertRepository(
-      localDataSource: AlertLocalDataSource(),
-      exceptionHandler: AlertExceptionHandler(),
-      remoteDataSource: AlertRemoteDataSource(),
-      preferencesLocalDataSource: PreferencesLocalDataSource());
+  IPreferenceRepository preferenceRepository = PreferenceRepository(
+      preferencesLocalDataSource: PreferencesLocalDataSource(),
+      exceptionHandler: PreferencesExceptionHandler());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,9 +54,26 @@ class _HomePageState extends State<HomePage> {
         children: [
           ElevatedButton(
               onPressed: () async {
-               var alerts = await alertRepo.getAllAlerts();
-              alerts.when(success: (data) {
-                  print("success ${data.length}}");
+                /*
+ var result = await preferenceRepository
+                    .setBaseCurrencyPreference("btc");
+                result.when(success: (_) {
+                  print("success result");
+                }, failure: (fail) {
+                  print("fail ${fail.message}");
+                });
+            
+    */
+          
+                final String localJsonPath = "assets/json/vs_currency.json";
+                var localData = await rootBundle.loadString(localJsonPath);
+                List<String> decodedData = jsonDecode(localData);
+                print(decodedData);
+
+                var crpreference =
+                    await preferenceRepository.getAllSupportedBaseCurrencies();
+                crpreference.when(success: (data) {
+                  print("success ${data.length}");
                 }, failure: (failure) {
                   print("failure ${failure.message}");
                 });
