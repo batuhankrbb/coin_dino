9e33add58e446f68893954fd74a55f89bdf39e28
Sercan Kaya <sercan.kaya@icloud.com>
2021-05-24 16:47:44 +0300

    added : LoadingScreen

---
+++ lib/global/components/loading_screen_components.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10), color: Colors.red),
+        )
+      ],
+    );
+  }
+}
