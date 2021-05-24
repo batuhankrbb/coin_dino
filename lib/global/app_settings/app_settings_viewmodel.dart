1970328d7eeb5d101ad4c922379ea3afad798ce2
batuhankrbb <batuhankarababa@gmail.com>
2021-05-24 16:49:48 +0300

    update: app_settings

--- lib/global/app_settings/app_settings_viewmodel.dart
+++ lib/global/app_settings/app_settings_viewmodel.dart
@@ -1,5 +1,7 @@
 import 'package:coin_dino/features/preferences/data/repository/preference_repository.dart';
+import 'package:coin_dino/features/preferences/domain/repository_contract/i_preference_repository.dart';
 import 'package:coin_dino/global/starting_files/injection_container.dart';
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 part 'app_settings_viewmodel.g.dart';
 
@@ -7,8 +9,14 @@ class AppSettingsViewModel = _AppSettingsViewModelBase
     with _$AppSettingsViewModel;
 
 abstract class _AppSettingsViewModelBase with Store {
-  final preferenceRepository = getit.get<PreferenceRepository>();
-  // LanguagePreferenceEntity languagePreferenceEntity = LanguagePreferenceEntity.turkish;
+  IPreferenceRepository preferenceRepository =
+      getit.get<IPreferenceRepository>();
 
+  Future<ThemeData> getTheme() async {
+    var themeData = await preferenceRepository.getThemePreference();
+    themeData.when(success: (data) {}, failure: (failure) {});
+    return ThemeData();
+  }
+  // LanguagePreferenceEntity languagePreferenceEntity = LanguagePreferenceEntity.turkish;
 
 }
