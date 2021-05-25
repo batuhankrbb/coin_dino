7b8580a692b506e0acbfe103c3f0d356bc0fac4a
Sercan Kaya <sercan.kaya@icloud.com>
2021-05-25 12:33:33 +0300

    added : EmptyDataComponents

---
+++ lib/global/components/empty_data_compnents.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EmptyDataComponents extends StatelessWidget {
+  const EmptyDataComponents({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(),
+    );
+  }
+}
