30d940bd79e533319bd574b2dfd6d50b05710045
batuhankrbb <batuhankarababa@gmail.com>
2021-05-26 12:36:50 +0300

    update: custom_autosize_text, selection_page

--- lib/global/components/custom_autosize_text.dart
+++ lib/global/components/custom_autosize_text.dart
@@ -1,20 +1,22 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
-
 //TODO DEVAM EDİLECEK
 class CustomAutoSizeText extends StatelessWidget {
-  CustomAutoSizeText({Key? key, required this.text, required this.textStyle, this.maxLines}) : super(key: key);
+  CustomAutoSizeText(
+      {Key? key, required this.text, required this.textStyle, this.maxLines, this.minFontSize = 14.0})
+      : super(key: key);
 
   String text;
   TextStyle textStyle;
   int? maxLines;
+  double minFontSize;
 
   @override
   Widget build(BuildContext context) {
     return AutoSizeText(
       text,
-      minFontSize: 14,
+      minFontSize: minFontSize,
       maxLines: maxLines,
       style: textStyle,
     );
--- lib/global/components/selection_page.dart
+++ lib/global/components/selection_page.dart
@@ -1,6 +1,7 @@
+import 'package:coin_dino/core/user_interface/responsive_layout/widgets/informer_widget.dart';
+import 'package:coin_dino/global/components/custom_autosize_text.dart';
 import 'package:coin_dino/global/utils/custom_colors.dart';
 import 'package:flutter/material.dart';
-
 
 //TODO DEVAM EDİLECEK
 
@@ -19,12 +20,24 @@ class SelectionPage<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: ListView.builder(
-          itemBuilder: (context, index) {
-            return SelectionPageCell(text: dataList[index],isSelected: false,);
-          },
-          itemCount: dataList.length,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(8),
+          child: ListView.separated(
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return SelectionPageCell(
+                  text: dataList[index],
+                  isSelected: false,
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Container(
+                  height: 1,
+                  color: CustomColor.shared.backgroundDarkModeColor,
+                );
+              },
+              itemCount: dataList.length),
         ),
       ),
     );
@@ -32,21 +45,29 @@ class SelectionPage<T> extends StatelessWidget {
 }
 
 class SelectionPageCell extends StatelessWidget {
-  SelectionPageCell({Key? key, required this.text, required this.isSelected}) : super(key: key);
+  SelectionPageCell({Key? key, required this.text, required this.isSelected})
+      : super(key: key);
 
   String text;
   bool isSelected;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 12),
-      alignment: Alignment.center,
-      child: Text(
-        text,
-        style: TextStyle(
-            fontSize: 30, color: CustomColor.shared.backgroundBlueColor),
-      ),
-    );
+    return InformerWidget(onPageBuild: (context, screenInfo) {
+      return Container(
+        height: screenInfo.screenSize.height * 0.06,
+        margin: EdgeInsets.only(left: 6, top: 3, bottom: 3, right: 6),
+        alignment: Alignment.centerLeft,
+        child: CustomAutoSizeText(
+          text: text,
+          minFontSize: 14,
+          textStyle: TextStyle(
+              fontSize: 16,
+              color: CustomColor.shared.backgroundDarkModeColor,
+              fontWeight: FontWeight.w300),
+          maxLines: 1,
+        ),
+      );
+    });
   }
 }
