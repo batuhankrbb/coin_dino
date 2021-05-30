15ccd91ff27c7b92963cc9c105aa589de7a72662
Sercan Kaya <sercan.kaya@icloud.com>
2021-05-30 12:44:25 +0300

    added: SettingScreenInapp Widget GestureDetector, In App Screen

---
+++ lib/inapp_purchase_screen/inapp_purchase_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InAppPurchase extends StatelessWidget {
+  const InAppPurchase({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("sdsd"),
+    );
+  }
+}
--- lib/settings_screen/components/settings_iap_card.dart
+++ lib/settings_screen/components/settings_iap_card.dart
@@ -11,46 +11,51 @@ class SettingInAppPurchaseCardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return InformerWidget(
       onPageBuild: (context, screenInformation) {
-        return Container(
-          width: screenInformation.screenSize.width * 0.98,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              gradient:
-                  LinearGradient(colors: [Colors.orange, Colors.orangeAccent])),
-          child: Row(
-            children: [
-              Expanded(
-                  flex: 7,
-                  child: ImageHelper.shared.getAssetImage(imageName: "ad")),
-              Spacer(flex: 1),
-              Expanded(
-                flex: 30,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AutoSizeText("Reklamları Kaldır",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15)),
-                    AutoSizeText(
-                        "Reklamları kaldırar bize destek olabilirsiniz. Bizde sizler için uygulamayı geliştirmek için elimizden geleni yaparız.",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.normal)),
-                  ],
+        return GestureDetector(
+          onTap: (){
+            
+          },
+          child: Container(
+            width: screenInformation.screenSize.width * 0.98,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                gradient:
+                    LinearGradient(colors: [Colors.orange, Colors.orangeAccent])),
+            child: Row(
+              children: [
+                Expanded(
+                    flex: 7,
+                    child: ImageHelper.shared.getAssetImage(imageName: "ad")),
+                Spacer(flex: 1),
+                Expanded(
+                  flex: 30,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AutoSizeText("Reklamları Kaldır",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15)),
+                      AutoSizeText(
+                          "Reklamları kaldırar bize destek olabilirsiniz. Bizde sizler için uygulamayı geliştirmek için elimizden geleni yaparız.",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal)),
+                    ],
+                  ),
                 ),
-              ),
-              Spacer(flex: 1),
-              Expanded(
-                  flex: 2,
-                  child: Icon(
-                    Icons.chevron_right,
-                    color: Colors.white,
-                  ))
-            ],
+                Spacer(flex: 1),
+                Expanded(
+                    flex: 2,
+                    child: Icon(
+                      Icons.chevron_right,
+                      color: Colors.white,
+                    ))
+              ],
+            ),
           ),
         );
       },
