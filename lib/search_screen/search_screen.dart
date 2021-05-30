3d1c64b98e990d2c2dc8b08f65a792fb3ec424c2
batuhankrbb <batuhankarababa@gmail.com>
2021-05-30 07:36:46 +0300

    add: todo, search_screen

--- lib/search_screen/search_screen.dart
+++ lib/search_screen/search_screen.dart
@@ -1,3 +1,5 @@
+import 'package:coin_dino/global/components/app_bar_components.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -8,10 +10,25 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  late TextEditingController textController;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text(""),
+    return Scaffold(
+      appBar: appbarComponent(title: "Search"),
+      body: Container(
+        child: Column(
+          children: [CupertinoTextField(controller: textController,onChanged: (value){
+            //todo buraya istek vmden
+          },)],
+        ),
+      ),
     );
   }
 }
