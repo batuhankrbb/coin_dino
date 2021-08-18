a858edee6260b3f605990138ee27f84be5fa6d31
batuhankrbb <batuhankarababa@gmail.com>
2021-08-18 08:05:25 +0300

    update: admob_helper(interstitial ad method)

--- lib/core/mobile_ads/admob_helper.dart
+++ lib/core/mobile_ads/admob_helper.dart
@@ -16,4 +16,33 @@ class AdmobHelper {
         listener: listener ?? BannerAdListener(),
         request: request ?? AdRequest());
   }
+
+  Future<void> showInterstitialAd(
+      {required String interstitialID,
+      AdRequest? adRequest,
+      required Function(LoadAdError error) onAdFailed,
+      Function(InterstitialAd ad)? onAdDismissed,
+      Function(InterstitialAd ad)? onAdShowed}) async {
+    await InterstitialAd.load(
+      adUnitId: interstitialID,
+      request: adRequest ?? AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(onAdLoaded: (ad) {
+        ad.fullScreenContentCallback = FullScreenContentCallback(
+          onAdShowedFullScreenContent: (ad) {
+            if (onAdShowed != null) {
+              onAdShowed(ad);
+            }
+          },
+          onAdDismissedFullScreenContent: (ad) {
+            if (onAdDismissed != null) {
+              onAdDismissed(ad);
+            }
+          },
+        );
+        ad.show();
+      }, onAdFailedToLoad: (error) {
+        onAdFailed(error);
+      }),
+    );
+  }
 }
