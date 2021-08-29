081542df52643d90ec9b9d6a2d0b75f37acc26a1
batuhankrbb <batuhankarababa@gmail.com>
2021-08-29 17:07:37 +0300

    update: in_app_purchase_button(name), settings_iap_card(visibility)

--- lib/screen_in_app_purchase/components/in_app_purchase_button.dart
+++ lib/screen_in_app_purchase/components/in_app_purchase_button.dart
@@ -1,34 +1,44 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:coin_dino/global/app_settings/app_settings_viewmodel.dart';
+import 'package:coin_dino/global/starting_files/injection_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import '../../core/extensions/context_extensions.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 class InAppPurchaseButton extends StatelessWidget {
-  const InAppPurchaseButton({Key? key, required this.onTap}) : super(key: key);
+  InAppPurchaseButton({Key? key, required this.onTap}) : super(key: key);
 
   final VoidCallback onTap;
+
+  var appSettingsVM = getit.get<AppSettingsViewModel>();
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      child: Container(
-        width: context.getHeight(0.9),
-        child: ElevatedButton(
-          style: ButtonStyle(
-            backgroundColor:
-                MaterialStateProperty.all<Color>(context.colorScheme.surface),
+      child: Observer(builder: (_) {
+        return Container(
+          width: context.getHeight(0.9),
+          child: ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(context.colorScheme.surface),
+            ),
+            onPressed: null,
+            child: buildText(context),
           ),
-          onPressed: null,
-          child: buildText(context),
-        ),
-      ),
+        );
+      }),
     );
   }
 
   AutoSizeText buildText(BuildContext context) {
+    var buttonText = appSettingsVM.isPremium
+        ? "INAPP_SCREEN.BUY_BUTTON_PREMIUM_TEXT"
+        : "INAPP_SCREEN.BUY_BUTTON_TEXT";
     return AutoSizeText(
-      "INAPP_SCREEN.BUY_BUTTON_TEXT".tr(),
+      buttonText.tr(),
       style: TextStyle(
         color: context.theme.hintColor,
         fontSize: context.getWidth(0.06),
--- lib/screen_settings/components/settings_iap_card.dart
+++ lib/screen_settings/components/settings_iap_card.dart
@@ -1,4 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:coin_dino/global/app_settings/app_settings_viewmodel.dart';
+import 'package:coin_dino/global/starting_files/injection_container.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import '../../core/navigation/routes/navigation_route.dart';
 import '../../core/navigation/services/navigation_service.dart';
 import '../../screen_in_app_purchase/inapp_purchase_screen.dart';
@@ -10,43 +13,51 @@ import '../../core/extensions/context_extensions.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 class SettingInAppPurchaseCardWidget extends StatelessWidget {
-  const SettingInAppPurchaseCardWidget({Key? key}) : super(key: key);
+   SettingInAppPurchaseCardWidget({Key? key}) : super(key: key);
+
+  var appSettingsVM = getit.get<AppSettingsViewModel>();
 
   @override
   Widget build(BuildContext context) {
-    return InformerWidget(
-      onPageBuild: (context, screenInformation) {
-        return GestureDetector(
-          onTap: navigateToInAppScreen,
-          child: Container(
-            width: screenInformation.screenSize.width * 0.98,
-            padding: EdgeInsets.all(context.getWidth(0.02)),
-            decoration: buildBoxDecoration(context),
-            child: Row(
-              children: [
-                Expanded(
-                    flex: 7,
-                    child: ImageHelper.shared.getAssetImage(imageName: "ad")),
-                Spacer(flex: 1),
-                Expanded(
-                  flex: 30,
-                  child: buildTexts(context),
+    return Observer(builder: (context) {
+      return Visibility(
+        visible: !appSettingsVM.isPremium,
+        child: InformerWidget(
+          onPageBuild: (context, screenInformation) {
+            return GestureDetector(
+              onTap: navigateToInAppScreen,
+              child: Container(
+                width: screenInformation.screenSize.width * 0.98,
+                padding: EdgeInsets.all(context.getWidth(0.02)),
+                decoration: buildBoxDecoration(context),
+                child: Row(
+                  children: [
+                    Expanded(
+                        flex: 7,
+                        child:
+                            ImageHelper.shared.getAssetImage(imageName: "ad")),
+                    Spacer(flex: 1),
+                    Expanded(
+                      flex: 30,
+                      child: buildTexts(context),
+                    ),
+                    Spacer(flex: 1),
+                    Expanded(
+                      flex: 3,
+                      child: Icon(
+                        Icons.chevron_right,
+                        color: context.colorScheme.onSurface,
+                        size: context.getWidth(0.07),
+                      ),
+                    )
+                  ],
                 ),
-                Spacer(flex: 1),
-                Expanded(
-                  flex: 3,
-                  child: Icon(
-                    Icons.chevron_right,
-                    color: context.colorScheme.onSurface,
-                    size: context.getWidth(0.07),
-                  ),
-                )
-              ],
-            ),
-          ),
-        );
-      },
-    );
+              ),
+            );
+          },
+        ),
+      );
+    });
   }
 
   BoxDecoration buildBoxDecoration(BuildContext context) {
